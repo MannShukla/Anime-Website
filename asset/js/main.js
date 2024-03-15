e35585809e076c8ca382e338ae735058d5c02a8a
MannShukla <115017829+MannShukla@users.noreply.github.com>
2024-03-15 16:45:23 +0530

    Small Changes in the UI

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,16 @@
 
 
 
+function scrollHeader() {
+  const header = document.getElementById('header');
+  if (this.scrollY >= 40) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+
+
 var brandSwiper = new Swiper(".brand-swiper", {
    
     spaceBetween: 24,
